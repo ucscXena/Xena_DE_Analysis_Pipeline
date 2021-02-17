9b6c28278a35d028fe3d5358ac439d70b6f7afc3
jingchunzhu <jingchunzhu@gmail.com>
2021-02-17 10:07:30 -0800

    add expression_unit parameter related new files, missing from previous commit

---
+++ fields/DiffgexChoiceField.py
@@ -0,0 +1,24 @@
+
+from appyter.fields import Field
+
+class DiffgexChoiceField(Field):
+  ''' Represing a choice of string values with a combo box.
+  :param name: (str) A name that will be used to refer to the object as a variable and in the HTML form.
+  :param urlhashkey: (str) the key in url hashed object
+  :param label: (str) A human readable label for the field for the HTML form
+  :param description: (Optional[str]) A long human readable description for the field for the HTML form
+  :param choices: (Union[List[str], Set[str], Dict[str, str]]) A set of choices that are available for this field or lookup table mapping from choice label to resulting value
+  :param default: (str) A default value as an example and for use during prototyping
+  :param section: (Optional[str]) The name of a SectionField for which to nest this field under, defaults to a root SectionField
+  :param value: (INTERNAL Any) The raw value of the field (from the form for instance)
+  :param \**kwargs: Additional keyword arguments used by other fields
+  '''
+  def __init__(self, **kwargs):
+    super().__init__(**kwargs)
+
+  def constraint(self):
+    return self.raw_value is not None and self.raw_value in self.choices
+
+  @property
+  def safe_value(self):
+    return self.value
